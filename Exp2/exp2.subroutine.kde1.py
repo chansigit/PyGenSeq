bccac9fa3e1f7bdbfd24525a18af6009659db084
chansigit <chansigit@gmail.com>
2017-05-03 12:45:32 +0800

    add more hints

--- Exp2/exp2.subroutine.kde1.py
+++ Exp2/exp2.subroutine.kde1.py
@@ -23,28 +23,40 @@ d2_collection_l300000 = []
 # The smaller the p-value is the more likely that null hypo
 # is rejected. On the contrary, the larger the p-value is
 # the more likely that the null hypo get accepted.
+print("\n=============== seqLen=300 ===============")
 for d2 in open("null_len300/null_iid.d2.txt", "r"):
     d2_collection_l300.append(int(d2))
 d2_collection_l300 = np.array(d2_collection_l300)
+print("Lilliefors test:")
 print(lillifors(d2_collection_l300))
+print("D’Agostino and Pearson’s test:")
 print(normaltest(d2_collection_l300))
 
+print("\n=============== seqLen=3000 ===============")
 for d2 in open("null_len3000/null_iid.d2.txt", "r"):
     d2_collection_l3000.append(int(d2))
 d2_collection_l3000 = np.array(d2_collection_l3000)
+print("Lilliefors test:")
 print(lillifors(d2_collection_l3000))
+print("D’Agostino and Pearson’s test:")
 print(normaltest(d2_collection_l3000))
 
+print("\n=============== seqLen=30000 ===============")
 for d2 in open("null_len30000/null_iid.d2.txt", "r"):
     d2_collection_l30000.append(int(d2))
 d2_collection_l30000 = np.array(d2_collection_l30000)
+print("Lilliefors test:")
 print(lillifors(d2_collection_l30000))
+print("D’Agostino and Pearson’s test:")
 print(normaltest(d2_collection_l30000))
 
+print("\n=============== seqLen=300000 ===============")
 for d2 in open("null_len300000/null_iid.d2.txt", "r"):
     d2_collection_l300000.append(int(d2))
 d2_collection_l300000 = np.array(d2_collection_l300000)
+print("Lilliefors test:")
 print(lillifors(d2_collection_l300000))
+print("D’Agostino and Pearson’s test:")
 print(normaltest(d2_collection_l300000))
 
 sns.kdeplot(d2_collection_l300, label="seqLen=300")
